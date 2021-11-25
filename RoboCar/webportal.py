0e8fa0b2d3588c957e04514659c07f4808774183
juanjuan7699 <71871190+juanjuan7699@users.noreply.github.com>
2021-11-25 13:00:37 +0800

    update log page

--- RoboCar/webportal.py
+++ RoboCar/webportal.py
@@ -1,43 +1,39 @@
 from flask import Flask, render_template, url_for, request
+from sqlalchemy.engine import create_engine
 
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField
 
+import sqlalchemy as db
+import sqlite3 as sql
+
+#conn = sqlite3.connect('Robocar.db')
+print ("Opened database successfully");
+
+#conn.execute('CREATE TABLE logs_page (distance_travelled TEXT, rotation TEXT, Obs_distance TEXT, time_spent TEXT)')
+print ("Table created successfully");
+#conn.close()
+
+#engine = create_engine('sqlite://Robocar.db')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Robocar.db'
+#app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite://Robocar.db'
 db = SQLAlchemy(app)
 
-class ConfigForm(FlaskForm):
-    MaxSpeed = IntegerField('Maximum Speed of Robotic Car')
-    RotationSpeed = IntegerField('Rotation Speed of Robotic Car')
-    ObstacleDistance = IntegerField('Obstacle Distance')
-    submit = SubmitField('Submit')
+# connection = engine.connect()
+# metadata = db.metadata()
+# logs = db.Table('logs', metadata, autoload=True, autoload_with = engine)
 
-from flask import Flask, render_template, url_for, request
-# from flask_nav import Nav
-# from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
+# print(logs.columns.key())
 
-# from flask_sqlalchemy import SQLAlchemy
 
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-
-app = Flask(__name__)
-# nav = Nav(app)
-#db = SQLAlchemy(app)
-
-# nav.register_element('my_navbar', Navbar(
-#     'thenav',
-#     View('Home Page', 'Main')))
 
 class ConfigForm(FlaskForm):
     MaxSpeed = IntegerField('Maximum Speed of Robotic Car')
     RotationSpeed = IntegerField('Rotation Speed of Robotic Car')
     ObstacleDistance = IntegerField('Obstacle Distance')
     submit = SubmitField('Submit')
-
 
 @app.route("/")
 
@@ -47,37 +43,27 @@ def main():
 
 @app.route("/logs")
 def logs():
-    return render_template('logs.html', title='Logs')
+    conn=sql.connect("Robocar.db")
+    conn.row_factory = sql.Row
 
-@app.route("/logs/log1")
-def logdetails():
-    return render_template('log1.html', title='Log1')
+    cur=conn.cursor()
+    cur.execute("select * from logs_page order by logID DESC")
 
-@app.route("/tutorials")
-def tutorials():
-    return render_template('tutorials.html', title='Tutorials')
+    rows=cur.fetchall()
+    return render_template('logs.html', title='Logs', rows = rows)
+
+@app.route("/logs/<logID>")
+def logdetails(logID):
+    conn=sql.connect("Robocar.db")
+    conn.row_factory = sql.Row
+
+    cur=conn.cursor()
+    cur.execute("select * from logs_page where logID = ?", logID)
+
+    rows=cur.fetchall()
+    print (rows)
+    return render_template('log1.html', title='Log ' + logID, rows = rows)
     
-@app.route("/tutorials/tutorial1")
-def tutorialdetails():
-    return render_template('tutorial1.html', title='Tutorial1')
-    
-@app.route("/configuration", methods=["GET", "POST"])
-def configuration():
-    form = ConfigForm() 
-    return render_template('configuration.html', form=form, title='Configuration')
-    
-    
-#@app.route("/contact", methods=["GET", "POST"])
-#def contact():
-#    """Standard `contact` form."""
-#    form = ContactForm()
-#    if form.validate_on_submit():
-#        return redirect(url_for("success"))
-#    return render_template(
-#        "contact.jinja2",
-#        form=form,
-#        template="form-template"
-#    )
 
 @app.route("/tutorials")
 def tutorials():
