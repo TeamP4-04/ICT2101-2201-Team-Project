17f6beff415cc6a4fd9d890c52f01d66a7c71c96
yyloh <72189764+yyloh@users.noreply.github.com>
2021-12-03 14:12:38 +0800

    Integration of Blockly Commands with Virtual Map
    Results from executed Blockly Commands are now reflected on the Virtual Map.
    List of Commands are also stored in the Logs Table of Database.
    Outcomes of the game have been set. (Different Alert Messages for Different Scenarios)

--- RoboCar/webportal.py
+++ RoboCar/webportal.py
@@ -5,50 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-<<<<<<< Updated upstream
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Robocar.db'
-db = SQLAlchemy(app)
-=======
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Robocar.db'
+#db = SQLAlchemy(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Logs(db.Model):
     logID = db.Column(db.Integer, primary_key=True)
-    gameID = db.Column(db.Integer, db.ForeignKey('Game.gameID'), nullable=False)
-    carInformationID = db.Column(db.Integer, db.ForeignKey('CarInformation.carInformationID'), nullable=False)
-    commandsExecuted = db.Column(db.Text, nullable=False)
+    stageID = db.Column(db.Integer, nullable=False)
+    commands = db.Column(db.Text, nullable=False)
     
-    def __init__(self, logID, gameID, carsInformationID, commandsExecuted):
-        self.logID = logID
-        self.gameID = gameID
-        self.carsInformationID = carsInformationID
-        self.commandsExecuted = commandsExecuted
+    def __init__(self, stageID, commands):
+        self.stageID = stageID
+        self.commands = commands
 
-    
-class Game(db.Model):
-    gameID = db.Column(db.Integer, primary_key=True)
-    stageID = db.Column(db.Integer, db.ForeignKey('Stage.stageID'), nullable=False)
-    
-    def __init__(self, gameID, stageID):
-        self.gameID = gameID
-        self.stageID = stageID
- 
- 
-    
-class Stage(db.Model):
-    stageID = db.Column(db.Integer, primary_key=True)
-    start_point = db.Column(db.Text, nullable=False)
-    end_point = db.Column(db.Text, nullable=False)
-    checkpoint = db.Column(db.Text, nullable=False)
-    obstacles_location_list = db.Column(db.Text, nullable=False)
-    
-    def __init__(self, stageID, start_point, end_point, checkpoint, obstacles_location_list):
-        self.stageID = stageID
-        self.start_point = start_point
-        self.end_point = end_point
-        self.checkpoint = checkpoint
-        self.obstacles_location_list = obstacles_location_list
-  
     
 class CarInformation(db.Model):
     carInformationID = db.Column(db.Integer, primary_key=True)
@@ -58,7 +29,7 @@ class CarInformation(db.Model):
     time_spent = db.Column(db.Text, nullable=False)
     obstacle_position = db.Column(db.Text, nullable=False)
     car_point = db.Column(db.Text, nullable=False)
-    gameID = db.Column(db.Integer, db.ForeignKey('Game.gameID'), nullable=False)
+
     
 class Tutorial(db.Model):
     tutorialID = db.Column(db.Integer, primary_key=True)
@@ -72,15 +43,6 @@ class Configuration(db.Model):
     rotation_speed = db.Column(db.Text, nullable=False)
     object_detection_range = db.Column(db.Text, nullable=False)
 
-class Commands(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    commands = db.Column(db.Text)
-    
-    def __init__(self, commands):
-        self.commands = commands
- 
->>>>>>> Stashed changes
-
 #class ConfigForm(FlaskForm):
 #    MaxSpeed = IntegerField('Maximum Speed of Robotic Car')
 #    RotationSpeed = IntegerField('Rotation Speed of Robotic Car')
@@ -91,20 +53,18 @@ class Commands(db.Model):
 def home():
     if request.method == "POST":
         commands = request.get_data()
-        test = Commands(commands)
-
-        db.session.add(test)
+        logs = Logs(1,commands)
+        db.session.add(logs)
         db.session.commit()
         print(commands)
+
         return render_template('main.html', title='Main')
     else:    
         return render_template('main.html', title='Main')
 
-
 @app.route("/logs")
 def logs():
     return render_template('logs.html', title='Logs')
-
 
 @app.route("/tutorials")
 def tutorials():
@@ -119,18 +79,6 @@ def tutorialdetails():
 #    form = ConfigForm() 
 #    return render_template('configuration.html', form=form, title='Configuration')
     
-    
-#@app.route("/contact", methods=["GET", "POST"])
-#def contact():
-#    """Standard `contact` form."""
-#    form = ContactForm()
-#    if form.validate_on_submit():
-#        return redirect(url_for("success"))
-#    return render_template(
-#        "contact.jinja2",
-#        form=form,
-#        template="form-template"
-#    )
 
 if __name__ == '__main__':
     app.run(debug=True)
